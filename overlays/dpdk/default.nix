0ad482d5b145a8ea1b72e7be2ad7b0eaebb36e17
pharra <typechasing@gmail.com>
2023-08-22 20:38:01 +0800

    update dpdk

--- overlays/dpdk/default.nix
+++ overlays/dpdk/default.nix
@@ -1,6 +1,7 @@
 self: super: {
   dpdk = super.dpdk.overrideAttrs (oldAttrs: rec {
     dpdkVersion = "23.07";
+    version = "${dpdkVersion}" + lib.optionalString mod "-${kernel.version}";
     src = fetchurl {
       url = "https://fast.dpdk.org/rel/dpdk-${dpdkVersion}.tar.xz";
       sha256 = "sha256-3gdkZfcXSg1ScUuQcuSDenJrqsgtj+fcZEytXIz3TUw=";
